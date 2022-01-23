3b4189c45d793075edd49b9a7641abe24de37065
Arman Cherkharov <arman1231@yandex.ru>
2022-01-23 15:19:06 +0300

    make bug fixes after review

--- scripts/script.js
+++ scripts/script.js
@@ -5,27 +5,29 @@ const closeBtn = document.querySelector(".modal__close-btn");
 const form = document.querySelector(".modal__form");
 let nameInput = document.querySelector("#modal__name");
 let jobInput = document.querySelector("#modal__title");
+let profileName = document.querySelector(".profile__name");
+let profileTitle = document.querySelector(".profile__title");
 
-openBtn.addEventListener("click", () => {
-  modal.classList.add("modal_opnd");
-});
-closeBtn.addEventListener("click", () => {
-  modal.classList.remove("modal_opnd");
-});
-modal.addEventListener("click", (e) => {
-  if (e.currentTarget === e.target) {
-    modal.classList.remove("modal_opnd");
-  }
-});
-
+const openModal = () => {
+  modal.classList.add("modal_opened");
+};
+const closeModal = () => {
+  modal.classList.remove("modal_opened");
+};
+openBtn.addEventListener("click", openModal);
+closeBtn.addEventListener("click", closeModal);
+// modal.addEventListener("click", (e) => {
+//   if (e.currentTarget === e.target) {
+//     modal.classList.remove("modal_opened");
+//   }
+// });
 const formSubmitHandler = (e) => {
   e.preventDefault();
   let nameInputVal = nameInput.value;
   let jobInputVal = jobInput.value;
-  let profileName = document.querySelector(".profile__name");
-  let profileTitle = document.querySelector(".profile__title");
+
   profileName.textContent = nameInputVal;
   profileTitle.textContent = jobInputVal;
-  modal.classList.remove("modal_opnd");
+  closeModal();
 };
 form.addEventListener("submit", formSubmitHandler);
